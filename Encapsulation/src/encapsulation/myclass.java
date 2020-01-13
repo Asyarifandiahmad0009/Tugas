735def14d59f0bf74646082dad9a195334162476
Asy'ari fandi ahmad <56857036+Asyarifandiahmad0009@users.noreply.github.com>
2020-01-13 18:46:25 +0700

    Add files via upload

---
+++ Encapsulation/src/encapsulation/myclass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+/**
+ *
+ * @author asy'ari
+ */
+public class myclass {
+  public static void main(String[] args) {
+    Encapsulation myObj = new Encapsulation();
+    myObj.setName("John"); // Set the value of the name variable to "John"
+    System.out.println(myObj.getName());
+  }
+}
+
