f1e88ea527fe6ad25b7b6de9bae59090cfba8f80
Asy'ari fandi ahmad <56857036+Asyarifandiahmad0009@users.noreply.github.com>
2020-01-14 11:56:24 +0700

    Add files via upload

---
+++ parfum/wangian.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 14 Jan 2020 pada 05.55
+-- Versi Server: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wangian`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `parfum`
+--
+
+CREATE TABLE `parfum` (
+  `serial_parfum` int(15) NOT NULL,
+  `produk` varchar(45) NOT NULL,
+  `nama_parfum` varchar(45) NOT NULL,
+  `pembuat` varchar(45) NOT NULL,
+  `alamat` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
